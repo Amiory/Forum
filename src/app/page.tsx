69bb81d22f44f597d7a33830c9c565605643d3d4
Amiory <amiory05@gmail.com>
2025-02-10 00:41:17 +0330

    Update: - changed header design slightly

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,11 +6,11 @@ export default function Home() {
   return (
     <div className="w-full p-6 space-y-6 ">
       <div className="w-full hidden rounded-b-3xl mx-auto text-center bg-lightBrown lg:flex items-center p-4 justify-center gap-10">
-        <HeaderIcon className="w-1/6" />
         <BoardHoleIcon className="absolute -top-7 w-3 -translate-y-1/2 right-3 " />
-        <h1 className="text-4xl text-darkBrown font-jacquard_24">Forum</h1>
-        <BoardHoleIcon className="absolute -top-7 w-3 -translate-y-1/2 left-3 -scale-x-100" />
         <HeaderIcon className="w-1/6" />
+        <h1 className="text-4xl text-darkBrown font-jacquard_24">Forum</h1>
+        <HeaderIcon className="w-1/6" />
+        <BoardHoleIcon className="absolute -top-7 w-3 -translate-y-1/2 left-3 -scale-x-100" />
       </div>
       <ForumPreview />
       <ForumPreview />
